182b51b28b807a8ac024ff840256eeba771c6f1f
ÐAPavlova <sakuranasty@mail.ru>
2019-03-14 02:24:57 +0300

    added js,problems with ham buttons

---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+$('.carousel').carousel({
+    interval: 2000
+})
+let searchIcon=$('#main-search button');
+let searchField=$('#main-search input');
+searchIcon.on('focusin',function () {
+    searchField.css('display','inline-block');
+});
+// $('.hamburger').on('click',function () {
+//     $('.nav').slideDown();
+// });
